5e2b8eeadebe1f03ac81ff81222088b86993431e
Сергей Караваев <serzh-karavaev-s-k@mail.ru>
2023-06-17 14:42:30 +0300

    Third

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -26,7 +26,8 @@ class RecipeIngredientsInLine(admin.TabularInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name", "author", "text", "cooking_time", "image", "date")
+    list_display = ("pk", "name", "author", "text", "cooking_time", "image",
+                    "date")
     search_fields = ("name", "author", "text", "cooking_time")
     list_filter = ("name", "author", "tags")
     readonly_fields = ("favarite_count",)
