7cd90025b67c7cc499190998c91530b734a70ba7
Сергей Караваев <serzh-karavaev-s-k@mail.ru>
2023-06-18 22:55:41 +0300

    QQQQQ

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -1,20 +1,38 @@
-from django.db.models import Sum
-from django.http import HttpResponse
+from io import BytesIO
+
+from django.http import FileResponse
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 
 from api.serializers import RecipeIngredients
 
 
 def download_cart(request):
     ingredients = RecipeIngredients.objects.filter(
-        recipe__shopping__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').annotate(
-                amount=Sum('amount'))
-    text = ''
-    for ingredient in ingredients:
-        text += (f'•  {ingredient["ingredient__name"]}'
-                 f'({ingredient["ingredient__measurement_unit"]})'
-                 f'— {ingredient["amount"]}\n')
-    headers = {
-        'Content-Disposition': 'attchment; filename=shoping_cart.txt'}
-    return HttpResponse(
-        text, content_type='text/plain; charset=UTF-8', headers=headers)
+        recipe__shopping__user=request.user
+    ).values_list("ingredient__name", "ingredient__measurement_unit", "amount")
+    cart_list = {}
+    for item in ingredients:
+        name = item[0]
+        if name not in cart_list:
+            cart_list[name] = {"measurement_unit": item[1], "amount": item[2]}
+        else:
+            cart_list[name]["amount"] += int(item[2])
+    height = 700
+    buffer = BytesIO()
+    pdfmetrics.registerFont(TTFont("arial", "arial.ttf"))
+    page = canvas.Canvas(buffer)
+    page.setFont("arial", 13)
+    page.drawString(100, 750, "Список покупок")
+    for i, (name, data) in enumerate(cart_list.items(), start=1):
+        page.drawString(
+            80, height,
+            f"{i}. {name} – {data['amount']} {data['measurement_unit']}"
+        )
+        height -= 25
+    page.showPage()
+    page.save()
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True,
+                        filename="shopping_list.pdf")
