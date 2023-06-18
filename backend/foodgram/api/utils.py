dec0dae10e476cf794bd52ae3030de404498f2fd
Сергей Караваев <serzh-karavaev-s-k@mail.ru>
2023-06-18 06:23:05 +0300

    QQQQQQQ

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -1,19 +1,26 @@
 from django.http import HttpResponse
+from datetime import datetime as dt
 
-from api.serializers import RecipeIngredients
 
+def download_cart(self, request, ingredients):
+    user = self.request.user
+    filename = f'{user.username}_shopping_list.txt'
 
-def download_cart(request):
-    ingredients = RecipeIngredients.objects.filter(
-        recipe__shopping__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').annotate(
-                amount=sum('amount'))
-    text = ''
-    for ingredient in ingredients:
-        text += (f'•  {ingredient["ingredient__name"]}'
-                 f'({ingredient["ingredient__measurement_unit"]})'
-                 f'— {ingredient["amount"]}\n')
-    headers = {
-        'Content-Disposition': 'attchment; filename=shoping_cart.txt'}
-    return HttpResponse(
-        text, content_type='text/plain; charset=UTF-8', headers=headers)
+    today = dt.today()
+    shopping_list = (
+        f'Список покупок для пользователя: {user.username}\n\n'
+        f'Дата: {today:%Y-%m-%d}\n\n'
+    )
+    shopping_list += '\n'.join([
+        f'- {ingredient["ingredient__name"]} '
+        f'({ingredient["ingredient__measurement_unit"]})'
+        f' - {ingredient["amount"]}'
+        for ingredient in ingredients
+    ])
+    shopping_list += f'\n\nFoodgram ({today:%Y})'
+
+    response = HttpResponse(
+        shopping_list, content_type='text.txt; charset=utf-8'
+    )
+    response['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
