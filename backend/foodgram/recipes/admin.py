15a44b00b71970a445f9b006e6e69d9c205da0ca
Сергей Караваев <serzh-karavaev-s-k@mail.ru>
2023-06-19 23:54:08 +0300

    DDD

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -28,11 +28,10 @@ class RecipeIngredientsInLine(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("pk", "name", "author", "text", "cooking_time", "image",
                     "date")
-    search_fields = ("recipe_name", "recipe_author", "text", "cooking_time")
+    search_fields = ("name", "author", "text", "cooking_time")
     list_filter = ("name", "author", "tags")
     readonly_fields = ("favarite_count",)
     inlines = (RecipeIngredientsInLine,)
-    raw_id_fields = ("name",)
 
     def favarite_count(self, obj):
         return obj.favorites.count()
