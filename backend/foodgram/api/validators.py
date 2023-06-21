1c9d4a9a080b2ec55bb1cb7d596cb0d0e2f81419
Сергей Караваев <serzh-karavaev-s-k@mail.ru>
2023-06-21 07:27:30 +0300

    DDD

--- backend/foodgram/api/validators.py
+++ backend/foodgram/api/validators.py
@@ -35,6 +35,6 @@ def validate_cooking_time(value):
     if not value:
         raise ValidationError("Нужно написать время приготовления")
     for item in range(value):
-        if item["amount"] <= 0:
+        if str(item["amount"]) <= 0:
             raise ValidationError("Приготовление занимает не менее 1 мин")
     return value
